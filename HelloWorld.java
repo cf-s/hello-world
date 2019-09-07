be7a236dfe2b06cfa6fa6f09632a4397f79fc663
Mark Williams <28856838+cf-s@users.noreply.github.com>
2019-09-07 17:23:11 +1000

    Added fizzbuzz

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,19 @@
 public class HelloWorld {
 
   public static void main (String[] args){
-    System.out.println("Hello World");
+
+    System.out.println("Hello World!");
+
+    fizzBuzz();
+  }
+
+  public static void fizzBuzz(){
+    for (int i = 1; i<101; i++){
+      if(i%3 == 0) System.out.print("Fizz");
+      if(i%5 == 0) System.out.print("Buzz");
+      if (i%3!=0 && i%5!=0) System.out.print(i);
+      System.out.println();
+    }
   }
 }
 
